627fc6b0c650836a84188755b8f03e87895abb13
ByunE1956 <fpqp85456@naver.com>
2022-07-21 22:00:32 +0900

    Python Study Day3

--- ex03.py
+++ ex03.py
@@ -1,11 +1,20 @@
-print("안녕하세요"[0])
-print(len("안녕하세요"))
-
-print(15, "+",  4, "=", 15 + 4)
+str_input = input("숫자 입력>")
+num_input = float(str_input)
 
 print()
-print("3462를 17로 나누었을 때의")
-print("- 몫:", 3462 // 17)
-print("- 나머지:", 3462 % 17)
+print(num_input, "inch")
+print((num_input * 2.54), "cm")
 
-input(">>> ")
+str_input = input("원의 반지름 입력> ")
+num_input = float(str_input)
+print()
+print("반지름: ", num_input)
+print("둘레: ", 2 * 3.14 * num_input)
+print("넓이: ", 3.14 * num_input ** 2)
+
+a = input("문자열 입력> ")
+b = input("문자열 입력> ")
+
+print(a, b)
+a, b = b, a
+print(a, b)

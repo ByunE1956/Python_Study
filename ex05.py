b9c6b3633a315431ddfb5fae523a467bb618ab27
ByunE1956 <fpqp85456@naver.com>
2022-07-25 22:29:23 +0900

    Python Study Day5[Bool type]

---
+++ ex05.py
@@ -0,0 +1,14 @@
+print(10 == 100)
+print(10 != 100)
+print(10 > 100)
+print(10 < 100)
+print(10 >= 100)
+print(10 <= 100)
+
+print(True and True)
+print(True and False)
+print(False and False)
+
+print(True or True)
+print(False or True)
+print(False or False)

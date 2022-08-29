ff4b8d0cf0321e66d3662f596cabd5b30801b9e4
ByunE1956 <fpqp85456@naver.com>
2022-08-29 21:31:40 +0900

    Python Exercise Day1 [Print]

---
+++ Python_exercise_basic100/act1.py
@@ -0,0 +1,19 @@
+# Hello 출력하기
+print("Hello")
+
+print("Hello World")
+print("Hello", "World")
+
+print("Hello")
+print("World")
+
+print("'Hello'")
+
+print('"Hello"')
+print("\"Hello\"")
+
+print("\"!@#$%^&*()\'")
+
+print("C:\\Download\\\'hello\'.py")
+
+print("print(\"Hello\\nWorld\")")
